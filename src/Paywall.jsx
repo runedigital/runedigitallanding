1435d9dd532230bc3b49827b4e99a9cd948ba557
Runedigital Studio <services@runedigitaltudio.com>
2025-12-04 23:34:47 +1030

    FORCED FIX

---
+++ src/Paywall.jsx
@@ -0,0 +1,1 @@
+import React from 'react'; export default function Paywall({ children }) { return ( <> {} <div style={{ position: 'absolute', bottom: 40, right: 40, color: 'red', fontFamily: 'Courier New', fontWeight: 'bold', pointerEvents: 'none', zIndex: 999 }}> PREVIEW MODE </div> </> ) }
