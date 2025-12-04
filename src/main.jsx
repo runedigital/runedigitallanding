596fbaac4f697b3fb349a407bd37dd440761bdac
Runedigital Studio <services@runedigitaltudio.com>
2025-12-04 23:45:55 +1030

    FORCED FIX

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,1 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Canvas } from '@react-three/fiber'
-import App from './App'
-import Paywall from './Paywall'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Paywall>
-      <Canvas camera={{ position: [0, 0, 5], fov: 75 }} style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}>
-         <color attach="background" args={['#101010']} />
-         <App />
-      </Canvas>
-    </Paywall>
-   </React.StrictMode>,
-)
+import React from 'react'; import ReactDOM from 'react-dom/client'; import { Canvas } from '@react-three/fiber'; import App from './App'; import './index.css'; ReactDOM.createRoot(document.getElementById('root')).render( <React.StrictMode> <div style={{ width: '100vw', height: '100vh', background: '#000' }}> <Canvas camera={{ position: [0, 0, 5] }}> <color attach='background' args={['#151515']} /> <App /> </Canvas> </div> </React.StrictMode> )
