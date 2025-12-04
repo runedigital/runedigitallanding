837e86ac6f1d8400cf0a7f50ef9550de1b8c5c25
Runedigital Studio <services@runedigitaltudio.com>
2025-12-04 23:00:33 +1030

    Update site

--- src/gallery/DARK_MATTER.jsx
+++ src/gallery/DARK_MATTER.jsx
@@ -2,7 +2,7 @@
             import React from 'react'
             export default function Placeholder(props) {
                 return (
-                    <group {...props}>
+                     <group {...props}>
                         <mesh>
                             <boxGeometry args={[1, 1, 1]} />
                             <meshStandardMaterial color="red" wireframe />
